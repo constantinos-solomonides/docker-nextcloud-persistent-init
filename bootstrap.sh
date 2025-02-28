53e46a0405782d9c5552fff0da64201198b8d80a
Constantinos Solomonides <constantinos.st.solomonides@gmail.com>
2025-02-28 22:28:50 +0100

    Commit version 0.1.0
    * Use `.gitignore` to limit stored files to `mounts/shared/init.sh`
    * Remove old `mariadb:10.07`-related files
    * Update `bootstrap.sh` to work end-to-end
    * Fix typos in files, esp. incorrectly named / referenced variables
    * Implement E2E `init.sh` functionality to create and store contents

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,93 +1,203 @@
-#!/bin/bash
-set -u
-MARIADB_VERSION=${MARIADB_VERSION:-}
-HOST_BASE=${HOST_BASE:-/var}
-CONTAINER_BASE=${CONTAINER_BASE:-}
-REQUIRED_TOOLS=(
-    ${REQUIRED_TOOLS[@]:-}
-    docker
-    docker-compose
+#!/bin/bash -xu
+
+export E=${E:-}
+
+export NEXTCLOUD_ADMIN_USERNAME="${NEXTCLOUD_ADMIN_USERNAME:-nextclouddbadmin}"
+MARIADB_ROOT_PASSWORD="${MARIADB_ROOT_PASSWORD:-}"
+NEXTCLOUD_ADMIN_PASSWORD="${NEXTCLOUD_ADMIN_PASSWORD:-}"
+
+export LOCAL_BASE_DIRECTORY=$(dirname $(realpath $0))
+export CONTAINER_BASE_DIRECTORY="/media/backup"
+export MOUNTS_DIRECTORY="mounts"
+export MYSQL_BASE_DIRECTORY="mysql"
+export NEXTCLOUD_BASE_DIRECTORY="nextcloud"
+export SHARED_DIRECTORY="shared"
+export NEXTCLOUD_FQDN="${NEXTCLOUD_FQDN:-$(hostname)}"
+export SQL_INIT_FILENAME="${SQL_INIT_FILENAME:-mysql_init.sql}"
+
+
+export NEXTCLOUD_DIRECTORIES=$(
+    echo \
+        "var/www/html/data" \
+        "var/www/html" \
+        "var/log/apache2" \
+        "etc/apache2" \
+        "etc/ssl/certs/nextcloud" \
+    | sed -e "s/\s\+/#/g"
 )
-E=${E:-}
+export MYSQL_DIRECTORIES=$(
+    echo \
+        "var/lib/mysql" \
+        "var/log/mysql" \
+        "etc/mysql" \
+    | sed -e "s/\s\+/#/g"
+)
+export DONE_FILE_NAME="SETUP_COMPLETE"
 
+NEXTCLOUD_LOCAL_BASE='${LOCAL_BASE_DIRECTORY}/${MOUNTS_DIRECTORY}/${NEXTCLOUD_BASE_DIRECTORY}'
+MYSQL_LOCAL_BASE='${LOCAL_BASE_DIRECTORY}/${MOUNTS_DIRECTORY}/${MYSQL_BASE_DIRECTORY}'
+SHARED_LOCAL_BASE='${LOCAL_BASE_DIRECTORY}/${MOUNTS_DIRECTORY}/${SHARED_DIRECTORY}'
 
-function usage(){
-    cat 1>&2 <<END
-    $(basename ${0})
-    Bash script to initialise the nextcloud setup. It will build a mariadb container,
-    create a directory structure on the host and copy files from within the containers in
-    the host directories for persistency.
+function recreate_directories(){
+    ${E} sudo rm -rf ${LOCAL_BASE_DIRECTORY}/${MOUNTS_DIRECTORY}/${MYSQL_BASE_DIRECTORY}/*
+    ${E} sudo rm -rf ${LOCAL_BASE_DIRECTORY}/${MOUNTS_DIRECTORY}/${NEXTCLOUD_BASE_DIRECTORY}/*
+    ${E} sudo rm -rf ${LOCAL_BASE_DIRECTORY}/${MOUNTS_DIRECTORY}/${SHARED_DIRECTORY}/*${DONE_FILE_NAME}
 
-    Use:
-    ${0} [ -h | [-m MARIADB_VERSION]]
-    Option  METAVAR             Usage
-    -h      -                   This help message
-    -d      -                   Debug mode on
-    -n      -                   Dry-Run mode on
-    -m      MARIADB_VERSION     The MariaDB version to use
-    -b      HOST_BASE           The base directory for the ones to mount on the host
-    -B      CONTAINER_BASE      The base directory for mounting in containers
-END
-    exit $1
-}
-
-function create_host_directory_hierarchy(){
-    mkdir -p ${HOST_BASE}/nextcloud/data
-    mkdir -p ${HOST_BASE}/nextcloud/logs/database
-    mkdir -p ${HOST_BASE}/nextcloud/logs/nextcloud
-    mkdir -p ${HOST_BASE}/nextcloud/settings/certificates/nextcloud/
-    mkdir -p ${HOST_BASE}/nextcloud/settings/db
-    mkdir -p ${HOST_BASE}/nextcloud/settings/etc/apache2
-    mkdir -p ${HOST_BASE}/nextcloud/settings/etc/mysql
-    mkdir -p ${HOST_BASE}/nextcloud/settings/html
-}
-
-function verify_tools(){
-    # Verify that all necessary tools are installed
-    return_value=0
-    for tool in ${REQUIRED_TOOLS[@]}; do
-        command -v ${tool} || { echo 1>&2 "Tool ${tool} not found" ; return_value=1; }
+    for directory in $(sed -e 's/#/ /g' <<<${NEXTCLOUD_DIRECTORIES}); do
+        mkdir -p ${LOCAL_BASE_DIRECTORY}/${MOUNTS_DIRECTORY}/${NEXTCLOUD_BASE_DIRECTORY}/${directory}
     done
-    return ${return_value}
+    for directory in $(sed -e 's/#/ /g' <<<${MYSQL_DIRECTORIES}); do
+        mkdir -p ${LOCAL_BASE_DIRECTORY}/${MOUNTS_DIRECTORY}/${MYSQL_BASE_DIRECTORY}/${directory}
+    done
+
 }
 
-function build_containers(){
-    docker-compose build
+function output_nextcloud_data(){
+    set +x
+    echo "For nextcloud container"
+    echo "======================="
+    echo "  environment:"
+    echo '    NEXTCLOUD_FQDN: "${NEXTCLOUD_FQDN}"'
+    echo '    MARIADB_ROOT_PASSWORD: "${MARIADB_ROOT_PASSWORD}"'
+    echo '    LOCAL_BASE_DIRECTORY: "${LOCAL_BASE_DIRECTORY}"'
+    echo '    CONTAINER_BASE_DIRECTORY: "${CONTAINER_BASE_DIRECTORY}"'
+    echo '    NEXTCLOUD_BASE_DIRECTORY: "${NEXTCLOUD_BASE_DIRECTORY}"'
+    echo '    MOUNTS_DIRECTORY: "${MOUNTS_DIRECTORY}"'
+
+    echo "  volumes:" 
+    for directory in $(sed -e 's/#/ /g' <<<${NEXTCLOUD_DIRECTORIES}); do
+        echo "    - \"${NEXTCLOUD_LOCAL_BASE}/${directory}:/${directory}\""
+    done
+
+
+    echo "For nextcloud_init container"
+    echo "============================"
+    echo "  environment:"
+    echo '    NEXTCLOUD_FQDN: "any.seeker-odysseas.net"'
+    echo '    MARIADB_ROOT_PASSWORD: "${MARIADB_ROOT_PASSWORD}"'
+    echo '    LOCAL_BASE_DIRECTORY: "${LOCAL_BASE_DIRECTORY}"'
+    echo '    CONTAINER_BASE_DIRECTORY: "${CONTAINER_BASE_DIRECTORY}"'
+    echo '    NEXTCLOUD_BASE_DIRECTORY: "${NEXTCLOUD_BASE_DIRECTORY}"'
+    echo '    MOUNTS_DIRECTORY: "${MOUNTS_DIRECTORY}"'
+    echo '    SHARED_DIRECTORY: "${SHARED_DIRECTORY}"'
+    echo '    DONE_FILE_NAME: "${DONE_FILE_NAME}"'
+    echo '    NEXTCLOUD_DIRECTORIES: "${NEXTCLOUD_DIRECTORIES}"'
+
+    echo "  volumes:" 
+    NEXTCLOUD_REMOTE_BASE='${CONTAINER_BASE_DIRECTORY}/${NEXTCLOUD_REMOTE_BASE}'
+    for directory in $(sed -e 's/#/ /g' <<<${NEXTCLOUD_DIRECTORIES}); do
+        echo "    - \"${NEXTCLOUD_LOCAL_BASE}/${directory}:${NEXTCLOUD_REMOTE_BASE}/${directory}\""
+    done
+    echo "    - \"${SHARED_LOCAL_BASE}:\${CONTAINER_BASE_DIRECTORY}/\${SHARED_DIRECTORY}\""
 }
 
-function run_containers_to_copy_data(){
-    # TODO
-}
-function stop_containers(){
-    # TODO
+function output_mysql_data(){
+    echo "For mysql container"
+    echo "==================="
+    echo "  environment:"
+    echo '    MARIADB_ROOT_PASSWORD: "${MARIADB_ROOT_PASSWORD}"'
+    echo '    LOCAL_BASE_DIRECTORY: "${LOCAL_BASE_DIRECTORY}"'
+    echo '    CONTAINER_BASE_DIRECTORY: "${CONTAINER_BASE_DIRECTORY}"'
+    echo '    MYSQL_BASE_DIRECTORY: "${MYSQL_BASE_DIRECTORY}"'
+    echo '    MOUNTS_DIRECTORY: "${MOUNTS_DIRECTORY}"'
+
+    echo "  volumes:" 
+    for directory in $(sed -e 's/#/ /g' <<<${MYSQL_DIRECTORIES}); do
+        echo "    - \"${MYSQL_LOCAL_BASE}/${directory}:/${directory}\""
+    done
+
+    echo "For mysql_init container"
+    echo "========================"
+    echo "  environment:"
+    echo '    MARIADB_ROOT_PASSWORD: "${MARIADB_ROOT_PASSWORD}"'
+    echo '    NEXTCLOUD_ADMIN_USERNAME: "${NEXTCLOUD_ADMIN_USERNAME}"'
+    echo '    NEXTCLOUD_ADMIN_PASSWORD: "${NEXTCLOUD_ADMIN_PASSWORD}"'
+    echo '    LOCAL_BASE_DIRECTORY: "${LOCAL_BASE_DIRECTORY}"'
+    echo '    CONTAINER_BASE_DIRECTORY: "${CONTAINER_BASE_DIRECTORY}"'
+    echo '    MYSQL_BASE_DIRECTORY: "${MYSQL_BASE_DIRECTORY}"'
+    echo '    MOUNTS_DIRECTORY: "${MOUNTS_DIRECTORY}"'
+    echo '    SHARED_DIRECTORY: "${SHARED_DIRECTORY}"'
+    echo '    DONE_FILE_NAME: "${DONE_FILE_NAME}"'
+    echo '    MYSQL_DIRECTORIES: "${MYSQL_DIRECTORIES}"'
+    echo '    SQL_INIT_FILENAME="${SQL_INIT_FILENAME}"'
+
+    echo "  volumes:" 
+    MYSQL_REMOTE_BASE='${CONTAINER_BASE_DIRECTORY}/${MYSQL_BASE_DIRECTORY}'
+    for directory in $(sed -e 's/#/ /g' <<<${MYSQL_DIRECTORIES}); do
+        echo "    - \"${MYSQL_LOCAL_BASE}/${directory}:${MYSQL_REMOTE_BASE}/${directory}\""
+    done
+    echo "    - \"${SHARED_LOCAL_BASE}:\${CONTAINER_BASE_DIRECTORY}/\${SHARED_DIRECTORY}\""
+
 }
 
-function get_mariadb_version(){
-    # Only run this if MARIA_DB version is unset
-    [ -z "${MARIADB_VERSION}" ] || return
-    MARIADB_VERSION=$(
-        docker run --rm -t ubuntu:jammy \
-            bash -c 'apt-get update >/dev/null 2>&1; apt-cache madison mariadb-server | \
-                        cut -d "|" -f 2 | \
-                        sort -V'| \
-                        tail -n 1
-    )
+function output_cleanup_data(){
+    echo "For cleanup container"
+    echo "========================"
+    echo "  environment:"
+    echo '    MARIADB_ROOT_PASSWORD: "${MARIADB_ROOT_PASSWORD}"'
+    echo "  volumes:" 
+    echo "    - \"${MYSQL_LOCAL_BASE}/:${MYSQL_REMOTE_BASE}/\""
+    echo "    - \"${SHARED_LOCAL_BASE}:\${CONTAINER_BASE_DIRECTORY}/\${SHARED_DIRECTORY}\""
 }
 
-while getopts hdnm:b:B: flag; do
-    case "${flag}" in
-        h) usage 0;;
-        d) set -x;;
-        n) export E=echo;;
-        m) MARIADB_VERSION=${OPTARG};;
-        b) HOST_BASE=${OPTARG};;
-        B) CONTAINER_BASE=${OPTARG};;
-        *) usage 1;;
-    esac
-done
 
-verify_tools || exit 1
-get_mariadb_version
-build_containers
-run_containers_to_copy_data
-stop_containers
+function run_init(){
+    read -p "If you want to remove all stored data, type REMOVE: "
+    if [ "$REPLY" != "REMOVE" ]; then
+        return 1
+    fi
+    
+    recreate_directories
+    echo "Make sure the following folders are mounted appropriately, and the following variables set:"
+    output_nextcloud_data
+    output_mysql_data
+
+    echo ""
+}
+
+case "${1}" in
+    init)
+        run_init
+    ;;
+    start)
+        docker compose -f docker-compose.yml start
+    ;;
+    stop)
+        docker compose -f docker-compose.yml stop
+    ;;
+    up)
+        if [ -z "${MARIADB_ROOT_PASSWORD}" ]; then
+            read -s -r -p "MARIADB_ROOT_PASSWORD: " MARIADB_ROOT_PASSWORD
+            echo ""
+        fi
+
+        if [ -z "${NEXTCLOUD_ADMIN_PASSWORD}" ]; then
+            read -s -r -p "NEXTCLOUD_ADMIN_PASSWORD: " NEXTCLOUD_ADMIN_PASSWORD
+            echo ""
+        fi
+
+        export MARIADB_ROOT_PASSWORD
+        export NEXTCLOUD_ADMIN_PASSWORD
+
+        docker compose -f docker-compose.yml up --remove-orphans
+    ;;
+    down)
+        docker compose -f docker-compose.yml down
+    ;;
+    logs)
+        docker compose -f docker-compose.yml logs "${2}"
+    ;;
+    exec)
+        # shellcheck disable=SC2086,SC2068
+        docker compose -f docker-compose.yml exec -ti ${@:2}
+    ;;
+    restart)
+        docker compose -f docker-compose.yml restart "${2}"
+    ;;
+    reset)
+        RESET=true $0 start
+    ;;
+    *)
+        echo "Invalid option ${1}"
+    ;;
+esac
---
+++ mounts/shared/init.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -x
+
+# DONE
+# [x]. Create mysql and nextcloud setup functions
+# [x]. Use shopt to define the init
+# [x]. Default to no action
+# [x]. Identify where files are stored for database and `rsync`' them
+function init_mysql(){
+    set -x
+    DONE_FILE="${CONTAINER_BASE_DIRECTORY}/${SHARED_DIRECTORY}/mysql-${DONE_FILE_NAME}"
+    [ -e "${DONE_FILE}" ]  && return 0
+    sed -i -e '/datadir.*=/s/#//' /etc/mysql/mariadb.conf.d/50-server.cnf
+    cat > /docker-entrypoint-initdb.d/${SQL_INIT_FILENAME} <<END
+CREATE USER '${NEXTCLOUD_ADMIN_USERNAME}'@'%' IDENTIFIED BY '${NEXTCLOUD_ADMIN_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+GRANT ALL PRIVILEGES on nextcloud.* to '${NEXTCLOUD_ADMIN_USERNAME}'@'%';
+END
+    cat /docker-entrypoint-initdb.d/${SQL_INIT_FILENAME}
+    docker-entrypoint.sh mariadbd & sleep 10;
+
+    for directory in $(sed -e 's/#/ /g' <<<${MYSQL_DIRECTORIES}); do
+        echo "rsync-ing directory ${directory}"
+        ls ${directory}
+        target="${CONTAINER_BASE_DIRECTORY}/${MYSQL_BASE_DIRECTORY}/${directory}"
+        mkdir -p ${target}
+        rsync -avuP "/${directory}/" "${target}/"
+
+    done
+    touch "${DONE_FILE}"
+}
+
+function init_nextcloud(){
+
+    DONE_FILE="${CONTAINER_BASE_DIRECTORY}/${SHARED_DIRECTORY}/nextcloud-${DONE_FILE_NAME}"
+    [ -e "${DONE_FILE}" ]  && return 0
+    for directory in $(sed -e 's/#/ /g' <<<${NEXTCLOUD_DIRECTORIES}); do
+        echo "rsync-ing directory ${directory}"
+        target="${CONTAINER_BASE_DIRECTORY}/${NEXTCLOUD_BASE_DIRECTORY}/${directory}"
+        mkdir -p ${target}
+        rsync -avuP "/${directory}/" "${target}/"
+    done
+    touch "${DONE_FILE}"
+}
+
+while getopts mn OPTARG; do
+    case "${OPTARG}" in 
+        m)
+            init_mysql;
+        ;;
+        n) 
+            init_nextcloud;
+        ;;
+        *)
+            echo "No option ${OPTARG} available"
+        ;;
+    esac
+done
