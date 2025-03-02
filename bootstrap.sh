87c410e4f444e481383849739647a640dd5d5e20
root <root@tc.seeker-odysseas.net>
2025-03-02 17:48:47 +0100

    Add auto-configuration via environment variables for nextcloud

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,10 @@
 
 export E=${E:-}
 
-export NEXTCLOUD_ADMIN_USERNAME="${NEXTCLOUD_ADMIN_USERNAME:-nextclouddbadmin}"
+export NEXTCLOUD_ADMIN_USER="${NEXTCLOUD_ADMIN_USER:-nextclouddbadmin}"
 MARIADB_ROOT_PASSWORD="${MARIADB_ROOT_PASSWORD:-}"
 NEXTCLOUD_ADMIN_PASSWORD="${NEXTCLOUD_ADMIN_PASSWORD:-}"
+export MYSQL_PASSWORD="${MYSQL_PASSWORD:-${NEXTCLOUD_ADMIN_PASSWORD}}"
 
 export LOCAL_BASE_DIRECTORY=$(dirname $(realpath $0))
 export CONTAINER_BASE_DIRECTORY="/media/backup"
@@ -14,6 +15,10 @@ export NEXTCLOUD_BASE_DIRECTORY="nextcloud"
 export SHARED_DIRECTORY="shared"
 export NEXTCLOUD_FQDN="${NEXTCLOUD_FQDN:-$(hostname)}"
 export SQL_INIT_FILENAME="${SQL_INIT_FILENAME:-mysql_init.sql}"
+export MYSQL_DATABASE="nextcloud"
+export MYSQL_USER="${MYSQL_USER:-${NEXTCLOUD_ADMIN_USER}}"
+export MYSQL_HOST="database"
+
 
 
 export NEXTCLOUD_DIRECTORIES=$(
@@ -52,6 +57,8 @@ function recreate_directories(){
 
 }
 
+
+
 function output_nextcloud_data(){
     set +x
     echo "For nextcloud container"
@@ -59,6 +66,8 @@ function output_nextcloud_data(){
     echo "  environment:"
     echo '    NEXTCLOUD_FQDN: "${NEXTCLOUD_FQDN}"'
     echo '    MARIADB_ROOT_PASSWORD: "${MARIADB_ROOT_PASSWORD}"'
+    echo '    NEXTCLOUD_ADMIN_USER: "${NEXTCLOUD_ADMIN_USER}"'
+    echo '    NEXTCLOUD_ADMIN_PASSWORD: "${NEXTCLOUD_ADMIN_PASSWORD}"'
     echo '    LOCAL_BASE_DIRECTORY: "${LOCAL_BASE_DIRECTORY}"'
     echo '    CONTAINER_BASE_DIRECTORY: "${CONTAINER_BASE_DIRECTORY}"'
     echo '    NEXTCLOUD_BASE_DIRECTORY: "${NEXTCLOUD_BASE_DIRECTORY}"'
@@ -78,6 +87,10 @@ function output_nextcloud_data(){
     echo '    LOCAL_BASE_DIRECTORY: "${LOCAL_BASE_DIRECTORY}"'
     echo '    CONTAINER_BASE_DIRECTORY: "${CONTAINER_BASE_DIRECTORY}"'
     echo '    NEXTCLOUD_BASE_DIRECTORY: "${NEXTCLOUD_BASE_DIRECTORY}"'
+    echo '    MYSQL_USER: "${MYSQL_USER}"        '
+    echo '    MYSQL_PASSWORD: "${MYSQL_PASSWORD}"'
+    echo '    MYSQL_DATABASE: "${MYSQL_DATABASE}"'
+    echo '    MYSQL_HOST: "${MYSQL_HOST}"        '
     echo '    MOUNTS_DIRECTORY: "${MOUNTS_DIRECTORY}"'
     echo '    SHARED_DIRECTORY: "${SHARED_DIRECTORY}"'
     echo '    DONE_FILE_NAME: "${DONE_FILE_NAME}"'
@@ -110,7 +123,7 @@ function output_mysql_data(){
     echo "========================"
     echo "  environment:"
     echo '    MARIADB_ROOT_PASSWORD: "${MARIADB_ROOT_PASSWORD}"'
-    echo '    NEXTCLOUD_ADMIN_USERNAME: "${NEXTCLOUD_ADMIN_USERNAME}"'
+    echo '    NEXTCLOUD_ADMIN_USER: "${NEXTCLOUD_ADMIN_USER}"'
     echo '    NEXTCLOUD_ADMIN_PASSWORD: "${NEXTCLOUD_ADMIN_PASSWORD}"'
     echo '    LOCAL_BASE_DIRECTORY: "${LOCAL_BASE_DIRECTORY}"'
     echo '    CONTAINER_BASE_DIRECTORY: "${CONTAINER_BASE_DIRECTORY}"'
@@ -119,6 +132,10 @@ function output_mysql_data(){
     echo '    SHARED_DIRECTORY: "${SHARED_DIRECTORY}"'
     echo '    DONE_FILE_NAME: "${DONE_FILE_NAME}"'
     echo '    MYSQL_DIRECTORIES: "${MYSQL_DIRECTORIES}"'
+    echo '    MYSQL_USER: "${MYSQL_USER}"        '
+    echo '    MYSQL_PASSWORD: "${MYSQL_PASSWORD}"'
+    echo '    MYSQL_DATABASE: "${MYSQL_DATABASE}"'
+    echo '    MYSQL_HOST: "${MYSQL_HOST}"        '
     echo '    SQL_INIT_FILENAME="${SQL_INIT_FILENAME}"'
 
     echo "  volumes:" 
@@ -129,17 +146,6 @@ function output_mysql_data(){
     echo "    - \"${SHARED_LOCAL_BASE}:\${CONTAINER_BASE_DIRECTORY}/\${SHARED_DIRECTORY}\""
 
 }
-
-function output_cleanup_data(){
-    echo "For cleanup container"
-    echo "========================"
-    echo "  environment:"
-    echo '    MARIADB_ROOT_PASSWORD: "${MARIADB_ROOT_PASSWORD}"'
-    echo "  volumes:" 
-    echo "    - \"${MYSQL_LOCAL_BASE}/:${MYSQL_REMOTE_BASE}/\""
-    echo "    - \"${SHARED_LOCAL_BASE}:\${CONTAINER_BASE_DIRECTORY}/\${SHARED_DIRECTORY}\""
-}
-
 
 function run_init(){
     read -p "If you want to remove all stored data, type REMOVE: "
@@ -176,8 +182,14 @@ case "${1}" in
             echo ""
         fi
 
+        if [ -z "${MYSQL_PASSWORD}" ]; then
+            read -s -r -p "MYSQL_PASSWORD" MYSQL_PASSWORD
+            echo ""
+        fi
+
         export MARIADB_ROOT_PASSWORD
         export NEXTCLOUD_ADMIN_PASSWORD
+        export MYSQL_PASSWORD
 
         docker compose -f docker-compose.yml up --remove-orphans
     ;;
--- mounts/shared/init.sh
+++ mounts/shared/init.sh
@@ -10,18 +10,19 @@
 # []. Setup certificates
 # []. Do configuration fixes recommended
 # []. Connect with postfix
+
 function init_mysql(){
     set -x
     DONE_FILE="${CONTAINER_BASE_DIRECTORY}/${SHARED_DIRECTORY}/mysql-${DONE_FILE_NAME}"
     [ -e "${DONE_FILE}" ]  && return 0
     sed -i -e '/datadir.*=/s/#//' /etc/mysql/mariadb.conf.d/50-server.cnf
     cat > /docker-entrypoint-initdb.d/${SQL_INIT_FILENAME} <<END
-CREATE USER '${NEXTCLOUD_ADMIN_USERNAME}'@'%' IDENTIFIED BY '${NEXTCLOUD_ADMIN_PASSWORD}';
-CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-GRANT ALL PRIVILEGES on nextcloud.* to '${NEXTCLOUD_ADMIN_USERNAME}'@'%';
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+GRANT ALL PRIVILEGES on ${MYSQL_DATABASE}.* to '${MYSQL_USER}'@'%';
 END
     cat /docker-entrypoint-initdb.d/${SQL_INIT_FILENAME}
-    docker-entrypoint.sh mariadbd & sleep 10;
+    docker-entrypoint.sh mariadbd & sleep 20;
 
     for directory in $(sed -e 's/#/ /g' <<<${MYSQL_DIRECTORIES}); do
         echo "rsync-ing directory ${directory}"
@@ -38,6 +39,11 @@ function init_nextcloud(){
 
     DONE_FILE="${CONTAINER_BASE_DIRECTORY}/${SHARED_DIRECTORY}/nextcloud-${DONE_FILE_NAME}"
     [ -e "${DONE_FILE}" ]  && return 0
+    echo MYSQL_USER: "${MYSQL_USER}"
+    echo MYSQL_PASSWORD: "${MYSQL_PASSWORD}"
+    echo MYSQL_DATABASE: "${MYSQL_DATABASE}"
+    echo MYSQL_HOST: "${MYSQL_HOST}"
+    /entrypoint.sh apache2-foreground & sleep 10
     for directory in $(sed -e 's/#/ /g' <<<${NEXTCLOUD_DIRECTORIES}); do
         echo "rsync-ing directory ${directory}"
         target="${CONTAINER_BASE_DIRECTORY}/${NEXTCLOUD_BASE_DIRECTORY}/${directory}"
